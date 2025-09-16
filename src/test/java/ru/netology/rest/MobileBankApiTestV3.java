8c446716312b43ee4e0afcaa3327c672fbd0b928
akovr <akovr@yahoo.com>
2025-09-16 11:50:09 +0700

    add test RUB and USD to schema

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -23,6 +23,8 @@ class MobileBankApiTestV3 {
                 .contentType(ContentType.JSON)
                 .body("", hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
+                .body("[1].currency", equalTo("USD"))
+                .body("[2].currency", equalTo("RUB"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
         ;
     }
